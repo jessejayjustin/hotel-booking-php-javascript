dc09cc0d795c783f340cc39cdbf6688ad6240674
Jesse Sosmena <23520011+jessejayjustin@users.noreply.github.com>
2021-05-06 17:54:16 +0800

    first commit

---
+++ footer.php
@@ -0,0 +1,56 @@
+ <script>
+ 	$('.datepicker').datepicker({
+ 		format:"yyyy-mm-dd"
+ 	})
+ 	 window.start_load = function(){
+    $('body').prepend('<di id="preloader2"></di>')
+  }
+  window.end_load = function(){
+    $('#preloader2').fadeOut('fast', function() {
+        $(this).remove();
+      })
+  }
+
+ 	window.uni_modal = function($title = '' , $url=''){
+    start_load()
+    $.ajax({
+        url:$url,
+        error:err=>{
+            console.log()
+            alert("An error occured")
+        },
+        success:function(resp){
+            if(resp){
+                $('#uni_modal .modal-title').html($title)
+                $('#uni_modal .modal-body').html(resp)
+                $('#uni_modal').modal('show')
+                end_load()
+            }
+        }
+    })
+}
+window.alert_toast= function($msg = 'TEST',$bg = 'success'){
+      $('#alert_toast').removeClass('bg-success')
+      $('#alert_toast').removeClass('bg-danger')
+      $('#alert_toast').removeClass('bg-info')
+      $('#alert_toast').removeClass('bg-warning')
+
+    if($bg == 'success')
+      $('#alert_toast').addClass('bg-success')
+    if($bg == 'danger')
+      $('#alert_toast').addClass('bg-danger')
+    if($bg == 'info')
+      $('#alert_toast').addClass('bg-info')
+    if($bg == 'warning')
+      $('#alert_toast').addClass('bg-warning')
+    $('#alert_toast .toast-body').html($msg)
+    $('#alert_toast').toast({delay:3000}).toast('show');
+  }
+ </script>
+ <!-- Bootstrap core JS-->
+        <script src="admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="admin/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+        <script src="admin/assets/js/jquery.magnific-popup.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
